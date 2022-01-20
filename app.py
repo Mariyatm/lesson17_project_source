e0ded8c6e8fcf221b385bce60e728912d93b0c90
mariazks <mariazks@yandex-team.ru>
2022-01-20 20:10:11 +0300

    done

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+api = Api(app)
+movie_ns = api.namespace('movies')
+director_ns = api.namespace('directors')
+genre_ns = api.namespace('genres')
 
 
 class Movie(db.Model):
@@ -24,6 +28,7 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
+
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
@@ -35,6 +40,97 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
+class DirectorSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+
+class GenreSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+
+movies_schema = MovieSchema(many=True)
+directors_schema = DirectorSchema(many=True)
+gnres_schema = GenreSchema(many=True)
+
+
+@movie_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        director_id = request.args.get("director_id")
+        genre_id = request.args.get("genre_id")
+        res = Movie.query
+        if director_id is not None:
+            res = res.filter(Movie.director_id == director_id)
+        if genre_id is not None:
+            res = res.filter(Movie.genre_id == genre_id)
+        return movies_schema.dump(res), 200
+
+    def post(self):
+        req_json = request.json
+        new_movie = Movie(**req_json)
+        with db.session.begin():
+            db.session.add(new_movie)
+        return "", 201
+
+
+@movie_ns.route('/<int:uid>')
+class MovieView(Resource):
+    def get(self, uid: int):
+        try:
+            movie = Movie.query.get(uid)
+            return MovieSchema.dump(movie), 200
+        except Exception as e:
+            return "", 404
+
+    def delete(self, uid: int):
+        movie = Movie.query.get(uid)
+        if not movie:
+            return "", 404
+        db.session.delete(movie)
+        db.session.commit()
+        return "", 204
+
+
+@director_ns.route('/')
+class DirectorsView(Resource):
+    def get(self):
+        all_directors = Director.query.all()
+        return MovieSchema.dump(all_directors), 200
+
+    def post(self):
+        req_json = request.json
+        new_director = Director(**req_json)
+        with db.session.begin():
+            db.session.add(new_director)
+        return "", 201
+
+
+@genre_ns.route('/')
+class GenresView(Resource):
+    def get(self):
+        all_genres = Genre.query.all()
+        return MovieSchema.dump(all_genres), 200
+
+    def post(self):
+        req_json = request.json
+        new_genre = Genre(**req_json)
+        with db.session.begin():
+            db.session.add(new_genre)
+        return "", 201
 
 
 if __name__ == '__main__':
